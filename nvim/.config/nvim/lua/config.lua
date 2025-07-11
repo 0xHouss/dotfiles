f744322a3aeacd4071270ffa2b62d38dbe66d955
0xHouss <tkthoussam@gmail.com>
2025-07-11 11:53:32 +0100

    add cloak, and some keymaps

--- nvim/.config/nvim/lua/config.lua
+++ nvim/.config/nvim/lua/config.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set({ "n", "v" }, "<C-y>", '"+y', { noremap = true, silent = true })
+vim.keymap.set({ "n", "i", "v" }, "<C-s>", function()
+  vim.lsp.buf.format({ async = false })
+  vim.cmd("write")
+end, { noremap = true, silent = true })
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -7,9 +7,6 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = {
-      auto_install = true,
-    },
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "ts_ls", "rust_analyzer" },
