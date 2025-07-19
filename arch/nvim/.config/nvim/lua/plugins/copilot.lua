63f2abc1170da80ff56c251387664760379474b7
0xHouss <tkthoussam@gmail.com>
2025-07-19 23:45:33 +0100

    feat(arch): add some nvim lazy extras

---
+++ arch/nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,10 @@
+return {
+  "zbirenbaum/copilot.lua",
+  event = "InsertEnter",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true, -- 👈 enables suggestions as you type
+    },
+  },
+}
