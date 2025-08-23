1b19bf0e55136e0661981e4e6da78c5a39fcba99
0xHouss <tkthoussam@gmail.com>
2025-08-23 09:49:27 +0100

    bind <space><space> to find files

--- arch/nvim/.config/nvim/lua/0xHouss/plugins/snacks.lua
+++ arch/nvim/.config/nvim/lua/0xHouss/plugins/snacks.lua
@@ -60,12 +60,12 @@ return {
   },
   keys = {
     -- Top Pickers & Explorer
-    { "<leader><space>", function() Snacks.picker.smart() end,                                                  desc = "Smart Find Files" },
+    { "<leader><space>", function() Snacks.picker.files() end,                                                  desc = "Find Files" },
     { "<leader>,",       function() Snacks.picker.buffers() end,                                                desc = "Buffers" },
     { "<leader>/",       function() Snacks.picker.grep() end,                                                   desc = "Grep" },
     { "<leader>:",       function() Snacks.picker.command_history() end,                                        desc = "Command History" },
     { "<leader>n",       function() Snacks.picker.notifications() end,                                          desc = "Notification History" },
-    { "<leader>e",       function() Snacks.explorer() end,                                                      desc = "File Explorer" },
+    -- { "<leader>e",       function() Snacks.explorer() end,                                                      desc = "File Explorer" },
     -- find
     { "<leader>fb",      function() Snacks.picker.buffers() end,                                                desc = "Buffers" },
     { "<leader>fc",      function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end,                desc = "Find Config File" },
